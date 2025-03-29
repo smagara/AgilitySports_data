ff993c8c361a1ef34972eaecc7f6958265c53526
Steve M <smagara@hotmail.com>
2025-03-28 20:13:47 -0400

    Initial test of GitHub SQL CI/CD pipeline

--- System/popSportsPositionCodes.sql
+++ System/popSportsPositionCodes.sql
@@ -21,5 +21,32 @@ values
 ('NBA', 'PG', 'Point Guard'),
 ('NBA', 'SG', 'Shooting Guard')
 
+/* populate the PositionCodes table, for NFL */
+insert into dbo.PositionCodes (Sport, PositionCode, PositionDesc)
+values 
+('NFL', 'C', 'Center'),
+('NFL', 'CB', 'Cornerback'),
+('NFL', 'DB', 'Defensive Back'),
+('NFL', 'DE', 'Defensive End'),
+('NFL', 'DL', 'Defensive Lineman'),
+('NFL', 'DT', 'Defensive Tackle'),
+('NFL', 'FB', 'Fullback'),
+('NFL', 'G', 'Guard'),
+('NFL', 'ILB', 'Inside Linebacker'),
+('NFL', 'K', 'Kicker'),
+('NFL', 'LB', 'Linebacker'),
+('NFL', 'LS', 'Long Snapper'),
+('NFL', 'NT', 'Nose Tackle'),
+('NFL', 'OL', 'Offensive Lineman'),
+('NFL', 'OLB', 'Outside Linebacker'),
+('NFL', 'OT', 'Offensive Tackle'),
+('NFL', 'P', 'Punter'),
+('NFL', 'QB', 'Quarterback'),
+('NFL', 'RB', 'Running Back'),
+('NFL', 'S', 'Safety'),
+('NFL', 'TE', 'Tight End'),
+('NFL', 'WR', 'Wide Receiver');
+
 select *
 from dbo.PositionCodes
+where sport = 'nfl'
