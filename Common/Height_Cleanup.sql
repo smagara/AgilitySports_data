134f5d1a84d9e450ff90f40db8d0dd710c260ae8
Steve M <smagara@hotmail.com>
2025-08-30 17:27:30 -0400

    Remerge 'Height_Cleanup.sql' with GitHub CI/CD reenabled

---
+++ Common/Height_Cleanup.sql
@@ -0,0 +1,30 @@
+/* 
+	Update height fields to eliminate interior spaces.
+	The formats were not consistent between sports.
+	New format is 5'11" not 5' 11", for example.
+
+	Remerging because the SQL CI/CD GitHub action was disabled for the database normalization.
+*/
+
+Use AgilitySports;
+GO
+
+BEGIN TRAN
+
+	UPDATE NFL.Roster
+		SET Height = replace(Height, ' ', '')
+	OUTPUT
+		INSERTED.PlayerId,
+		INSERTED.LastName,
+		DELETED.Height AS Old_Height,
+		INSERTED.Height AS New_Height
+
+	UPDATE NBA.Roster
+		SET Height = replace(Height, ' ', '')
+	OUTPUT
+		INSERTED.PlayerId,
+		INSERTED.LastName,
+		DELETED.Height AS Old_Height,
+		INSERTED.Height AS New_Height
+
+COMMIT TRAN
